d253186e447c65a2ef1482d56d1f0da8bf63c8ce
Nagavenkateswararao Ede <nagavede@in.ibm.com>
2021-10-05 19:31:07 +0530

    Initial Check in for Singly Linked List.

--- 02-LinkedList/src/com/dsa/linkedlist/app/LinkedListApp.java
+++ 02-LinkedList/src/com/dsa/linkedlist/app/LinkedListApp.java
@@ -1,9 +1,23 @@
 package com.dsa.linkedlist.app;
 
+import com.dsa.linkedlist.SinglyLinkedList;
+
 public class LinkedListApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		SinglyLinkedList<Integer> tSinglyLinkedList = new SinglyLinkedList<>();
+
+		tSinglyLinkedList.insert(10);
+		tSinglyLinkedList.insert(50);
+		tSinglyLinkedList.insert(15);
+		tSinglyLinkedList.insert(1);
+		tSinglyLinkedList.insert(100);
+
+		tSinglyLinkedList.insert(33, 2);
+
+		System.out.println("The Size of the List is: " + tSinglyLinkedList.size());
+		
+		tSinglyLinkedList.printList();
 
 	}
 
