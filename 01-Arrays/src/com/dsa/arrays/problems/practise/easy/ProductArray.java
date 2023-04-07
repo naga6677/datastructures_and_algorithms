8055c3437fb00daa1f2fa20c8f3e9f00e131e1d9
DESKTOP-GK81JV6\IND <venkat26naga@gmail.com>
2023-04-07 23:48:28 +0530

    Product Of An Array.

---
+++ 01-Arrays/src/com/dsa/arrays/problems/practise/easy/ProductArray.java
@@ -0,0 +1,45 @@
+package com.dsa.arrays.problems.practise.easy;
+
+/**
+ * Given an array of integers, update every element with multiplication of
+ * previous and next elements with following exceptions. a) First element is
+ * replaced by multiplication of first and second. b) Last element is replaced
+ * by multiplication of last and second last. Input: arr[] = {2, 3, 4, 5, 6}
+ * Output: arr[] = {6, 8, 15, 24, 30}
+ * 
+ * @author IND
+ *
+ */
+public class ProductArray {
+
+	private void productOfElement(int arr[]) {
+		int previousElement = arr[0];
+
+		int arrlength = arr.length;
+
+		for (int i = 0; i < arrlength; i++) {
+			int temp = arr[i];
+			if (i == 0) {
+				arr[i] = arr[i] * arr[i + 1];
+
+			} else if (i == arrlength - 1) {
+				arr[i] = previousElement * arr[i];
+			} else {
+				arr[i] = previousElement * arr[i + 1];
+			}
+			previousElement = temp;
+		}
+	}
+
+	public static void main(String[] args) {
+		int arr[] = { 2, 3, 4, 5, 6 };
+		
+		ProductArray aProductArray = new ProductArray();
+		aProductArray.productOfElement(arr);
+		
+		for (int i : arr) {
+			System.out.println(i);
+		}
+	}
+
+}
