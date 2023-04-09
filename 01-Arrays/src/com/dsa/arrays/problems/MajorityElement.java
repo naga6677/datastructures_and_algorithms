c8cd00313cd4b44e627896234dc1ba25b6a8d496
DESKTOP-GK81JV6\IND <venkat26naga@gmail.com>
2023-04-09 18:40:28 +0530

    Majority Elements Problem.

---
+++ 01-Arrays/src/com/dsa/arrays/problems/MajorityElement.java
@@ -0,0 +1,53 @@
+package com.dsa.arrays.problems;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * https://leetcode.com/problems/majority-element/
+ * 
+ * The majority element is the element that appears more than ⌊n / 2⌋ times. You
+ * may assume that the majority element always exists in the array.
+ * 
+ * Example 1:
+ * 
+ * Input: nums = [3,2,3] Output: 3
+ * 
+ * @author IND
+ *
+ */
+public class MajorityElement {
+
+	public int majorityElement(int[] nums) {
+
+		int anArrayLength = nums.length;
+
+		int count = 0;
+		int candidate = 0;
+
+		for (int i = 0; i < anArrayLength; i++) {
+			int currentElement = nums[i];
+
+			if (count == 0) {
+				candidate = currentElement;
+			}
+
+			if (currentElement == candidate) {
+				count++;
+			} else {
+				count--;
+			}
+		}
+		return candidate;
+	}
+
+	public static void main(String[] args) {
+		MajorityElement majorityElement = new MajorityElement();
+
+		int arr[] = { 3, 2, 3, 2 };
+		int anElement = majorityElement.majorityElement(arr);
+
+		System.out.println("The Majority Element is: " + anElement);
+	}
+
+}
